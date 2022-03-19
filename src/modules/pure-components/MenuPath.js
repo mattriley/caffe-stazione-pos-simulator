7d7a655b346fce016cea2979c8bf4cdaff01ac84
Matt Riley <mattrileyaus@gmail.com>
2022-03-19 12:48:56 +1100

    Unspecified changes

--- src/modules/pure-components/MenuPath.js
+++ src/modules/pure-components/MenuPath.js
@@ -11,6 +11,10 @@ const MenuPath = ({ lib, menu }) => ({ item, onClick }) => {
     const pathItems = pathIncrements.map(path => {
         const fullPath = path.flatMap(p => ['tree', p]);
 
+        if (!_.get(menu.tree, fullPath)) {
+            console.log({ fullPath });
+        }
+
         const { labelSingleLine, backColor, textColor } = _.get(menu.tree, fullPath);
         const className = `path-segment back-color-${backColor} text-color-${textColor}`.toLowerCase();
         return <span className={className}>{labelSingleLine}</span>
