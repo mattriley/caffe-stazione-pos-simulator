dcd339ee457420f4c12a1199c68a04673db96f70
Matt Riley <mattrileyaus@gmail.com>
2022-03-22 18:47:37 +1100

    Unspecified changes

--- src/modules/pure-components/TitleBar.js
+++ src/modules/pure-components/TitleBar.js
@@ -5,7 +5,7 @@ const TitleBar = () => () => {
     return <header>
         <div className="title-bar">
             <div className="left">
-                <span className="app-name">Simulatore di Stazione</span>
+                <span className="app-name">Stazione Simulator</span>
                 <a target="_blank" href="https://www.linkedin.com/in/mattrileyau/">by Matt Riley</a>
             </div>
             <div className="right">
