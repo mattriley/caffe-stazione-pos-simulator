dcff8a3b8de2521927ee061598a3fd69ab7d2f31
Matt Riley <mattrileyaus@gmail.com>
2022-03-22 19:41:58 +1100

    Unspecified changes

--- src/modules/pure-components/SearchResults.js
+++ src/modules/pure-components/SearchResults.js
@@ -7,8 +7,6 @@ const SearchResults = ({ pureComponents }) => ({ items, onSelection, selectedIte
         return <pureComponents.MenuPath item={item} onClick={() => onSelection(item)} />
     });
 
-    // todo: if one result, center it...
-
     const className = searchResults.length === 1 ? 'single-search-result' : 'search-results';
 
     return <div className={className}>{searchResults}</div>
