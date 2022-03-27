be1fccb98476c2c5da474e6bcbe6974171535fec
Matt Riley <mattrileyaus@gmail.com>
2022-03-27 22:44:43 +1100

    Unspecified changes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,1 @@
-import './index.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import boot from './boot';
-import data from './data';
-
-const { config } = data;
-const { modules } = boot({ config, data });
-const { pureComponents, components, menuReader } = modules;
-
-window.document.title = config.app.name;
-
-ReactDOM.render(
-  <React.StrictMode>
-    {components.App({ pureComponents, components, menuReader })}
-  </React.StrictMode >,
-  document.getElementById('root')
-);
+import './app.js'
