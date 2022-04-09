2d9c2f63558836ae9282be8e4f132ae4de9a9519
Linus Karsai <4230159+karsai5@users.noreply.github.com>
2022-04-09 12:46:15 +1000

    Experiment with x / y button placement

--- src/modules/pure-components/MenuButton.js
+++ src/modules/pure-components/MenuButton.js
@@ -1,7 +1,11 @@
 const MenuButton = () => ({ item, selected = false }) => {
     const { label, backColor, textColor = 'black', span = 1 } = item;
     const buttonText = { __html: label.replaceAll('\n', '<br>') };
-    return <div className={`
+    const gridColumn = item.x ? `${item.x} / span ${span}` : undefined;
+    const gridRow = item.y ? `${item.y} / span 1` : undefined;
+    return <div 
+        style={{gridColumn, gridRow}}
+        className={`
         menu-button 
         span-${span} selected-${selected}
         back-color-${backColor}
