6cfddfb177ef0da6716bc4a6fed6e0387d70c40a
Matt Riley <mattrileyaus@gmail.com>
2022-04-09 10:32:56 +1000

    use dev-bar web component

--- src/modules/pure-components/DevBar.js
+++ src/modules/pure-components/DevBar.js
@@ -1,16 +1,8 @@
 const DevBar = ({ config }) => () => {
 
-    return <header>
-        <div className="dev-bar">
-            <div className="left">
-                <span className="app-name">{config.app.name}</span>
-                <a className="author" target="_blank" href={config.author.profile}>by {config.author.name}</a>
-            </div>
-            <div className="right">
-                <a target="_blank" href={config.app.source}>Source code</a>
-            </div>
-        </div>
-    </header>
+    return <dev-bar app-name={config.app.name}>
+        <a target="_blank" href={config.app.source}>Source code</a>
+    </dev-bar>
 
 };
 
