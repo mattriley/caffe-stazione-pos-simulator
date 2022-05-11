972638f3a0a537c934ed1431a5faa195f822c7ca
Matt Riley <mattrileyaus@gmail.com>
2022-05-11 10:47:10 +1000

    Unspecified changes

--- tasks/lib/readme-file-generator.js
+++ tasks/lib/readme-file-generator.js
@@ -1,17 +1,27 @@
+import fs from 'fs';
 import ejs from 'ejs';
 import child from 'child_process';
 import process from 'process';
-import compose from '../../src/compose';
+import compose from './src/compose';
 
 const fetchText = url => child.execSync(`curl ${url}`).toString('utf8');
 
 const fetchCode = (url, opts = {}) => {
-    const open = opts.open || true;
     const code = fetchText(url);
+    return renderCode(code, url, opts);
+};
+
+const readCode = (path, opts = {}) => {
+    const code = fs.readFileSync(path, 'utf-8');
+    return renderCode(code, path, opts);
+};
+
+const renderCode = (code, summary, opts = {}) => {
+    const open = opts.open || true;
 
     return [
         `<details ${open ? 'open' : ''}>`,
-        `<summary>${url}</summary>`,
+        `<summary>${summary}</summary>`,
         '',
         '```js',
         (opts.includeFootnotes ? code : code.split('/*')[0]).trim(),
@@ -29,8 +39,8 @@ const moduleGraph = () => {
     ].join('\n');
 };
 
-const [templateFile = 'README-TEMPLATE.md'] = process.argv.slice(2);
-const data = { fetchText, fetchCode, moduleGraph };
+const [templateFile] = process.argv.slice(2);
+const data = { fetchText, fetchCode, readCode, moduleGraph };
 
 ejs.renderFile(templateFile, data, {}, (err, res) => {
     if (err) throw err;
