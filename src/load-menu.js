0d8616134e38bdbf03cbc7690f0098f9cccb3fa8
Matt Riley <mattrileyaus@gmail.com>
2022-05-18 08:55:03 +1000

    Unspecified changes

--- src/load-menu.js
+++ src/load-menu.js
@@ -14,39 +14,34 @@ const loadMenu = () => {
     const tree = {};
     const allItems = [];
 
-    const transformRecursive = (menu, path = []) => {
+    const transformRecursive = (menu, parentPath = []) => {
 
         const items = menu.items?.flatMap(item => {
 
             id++;
-
             const name = item.label.replaceAll('\n', '');
-
-
-            item.items = itemsReplacements[name] ?? item.items;
-
-
+            const items = itemsReplacements[name] ?? item.items;
             const words = name.split(' ');
             const keywords = Object.entries(abbreviations).reduce((acc, [abbr, full]) => {
                 return words.indexOf(abbr) > -1 ? acc.concat(full) : acc;
             }, item.keywords ?? []);
 
-            const newPath = path.concat(name);
+            const path = parentPath.concat(name);
 
-            const searchTerms = [...newPath, ...keywords]
+            const searchTerms = [...path, ...keywords]
                 .map(term => term.replace('W/', 'WITH'));
 
             const searchText = searchTerms.join(' ');
 
-            const pathArray = [...newPath];
+            const pathArray = [...path];
 
-            const newItem = { id, name, pathArray, searchText, keywords, ...item };
+            const newItem = { ...item, id, name, items, pathArray, searchText, keywords };
 
             allItems.push(newItem);
 
-            const setPath = newPath.flatMap(key => ['tree', key]).join('.');
+            const setPath = path.flatMap(key => ['tree', key]).join('.');
             _.set(tree, setPath, newItem);
-            const res = transformRecursive(newItem, newPath);
+            const res = transformRecursive(newItem, path);
             newItem.items = res.items;
             return newItem;
         });
