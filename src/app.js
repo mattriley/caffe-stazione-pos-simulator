cd42fb120b76a9e5afc9f936502c59f0fbb30d8f
Matt Riley <mattrileyaus@gmail.com>
2022-06-26 16:21:46 +1000

    Unspecified changes

--- src/app.js
+++ src/app.js
@@ -5,11 +5,10 @@ import compose from './compose.js';
 import loadMenu from './load-menu.js';
 
 const menu = loadMenu();
-const modules = compose({ menu });
-const { config, components } = modules;
+const { modules, config } = compose({ configs: [{ menu }] });
 window.app = modules;
 window.document.title = config.app.name;
 
 const container = document.getElementById('app');
 const root = createRoot(container);
-root.render(<React.StrictMode><components.App /></React.StrictMode>);
+root.render(<React.StrictMode><modules.components.App /></React.StrictMode>);
--- src/compose.js
+++ src/compose.js
@@ -2,13 +2,14 @@ import composer from 'module-composer';
 import modules from './modules/index.js';
 import defaultConfig from './default-config.js';
 
-export default (...configs) => {
+export default ({ configs }) => {
 
-    const { compose, config } = composer(modules, defaultConfig, ...configs);
+    const { compose, config } = composer(modules, { defaultConfig, configs });
     const { menu } = config;
     const { util } = compose('util');
     const { menuReader } = compose('menuReader', { menu, util });
     const { pureComponents } = compose('pureComponents', { menuReader, util, config });
-    return compose('components', { pureComponents, menuReader });
+    compose('components', { pureComponents, menuReader });
+    return compose.end();
 
 };
