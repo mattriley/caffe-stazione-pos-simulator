56ada653dd6ecc5ee2557fdeaf1ec2b7ab353164
Matt Riley <mattrileyaus@gmail.com>
2022-07-05 23:27:47 +1000

    Unspecified changes

--- src/modules/components/App.mjs
+++ src/modules/components/App.mjs
@@ -1,5 +1,5 @@
 export default ({ pureComponents, components, menuReader }) => () => {
-
+    console.log(4);
     return <div>
         <pureComponents.DevBar />
         <components.SearchScreen {...{ pureComponents, menuReader }} />
