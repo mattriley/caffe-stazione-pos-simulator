699b007aa48ddadfaad9d44b996edfbdb1607287
Matt Riley <mattrileyaus@gmail.com>
2022-07-05 23:23:23 +1000

    Unspecified changes

--- src/app.mjs
+++ src/app.mjs
@@ -4,12 +4,18 @@ import React from 'react';
 import compose from './compose.mjs';
 import loadMenu from './load-menu.mjs';
 
+console.log(1);
+
 const menu = loadMenu();
 const composition = compose({ configs: [{ menu }] });
 const { config, modules } = composition;
 window.app = composition;
 window.document.title = config.app.name;
 
+console.log(2);
+
 const container = document.getElementById('app');
 const root = createRoot(container);
 root.render(<React.StrictMode><modules.components.App /></React.StrictMode>);
+
+console.log(3);
