9594b0b79e1e9ccfd599e850234caa909801a3ab
Matt Riley <mattrileyaus@gmail.com>
2022-09-04 10:10:54 +1000

    Unspecified changes

---
+++ src/index.mjs
@@ -0,0 +1,12 @@
+// eslint-disable-next-line import/default
+import app from './app.js';
+import compose from './compose.mjs';
+import defaultConfig from './default-config.mjs';
+import modules from './modules/index.mjs';
+
+export default {
+    app,
+    compose,
+    defaultConfig,
+    modules
+};
