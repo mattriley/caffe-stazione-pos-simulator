68673f6196d026ee9743fc79565a84c926e25f81
Matt Riley <m@ttriley.dev>
2022-09-30 11:34:19 +1000

    Unspecified changes

--- tasks/_readme-gen.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo "Overridden to suppress error: exports is not defined in ES module scope"
--- tasks/code-gen.sh
+++ tasks/code-gen.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-echo "PACKAGE_NAME=$PACKAGE_NAME" > "./.env"
 node "$TASKS/bin/menu-gen.mjs" < "$DATA/menu.json" > "$STATIC/menu.json"
