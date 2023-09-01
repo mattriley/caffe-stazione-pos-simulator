e215888cfb51bf432a325ebd5c29820fc8e2332b
Matt Riley <m@ttriley.dev>
2023-09-02 09:19:54 +1000

    Unspecified changes

--- src/compose.mjs
+++ src/compose.mjs
@@ -1,14 +1,12 @@
-import composer from 'module-composer';
-import modules from './modules/index.mjs';
 import defaultConfig from './default-config.mjs';
+import modules from './modules/index.mjs';
+import composer from 'module-composer';
 
 export default ({ config } = {}) => {
 
-    const { configure } = composer(modules);
-    const { compose } = configure([defaultConfig, config]);
-
-    const { io } = compose('io', {});
-    const { menuReader } = compose('menuReader', {});
+    const { compose } = composer(modules, { config, defaultConfig });
+    const { io } = compose('io');
+    const { menuReader } = compose('menuReader');
     const { pureComponents } = compose('pureComponents', { menuReader });
     return compose('components', { pureComponents, menuReader, io });
 
